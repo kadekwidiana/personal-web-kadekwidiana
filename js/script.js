0e8975a9cd680f2b924b1b3aa64f087cc7b26de7
Kadek Widiana <kdkonos10@gmail.com>
2024-12-26 13:18:43 +0800

    update scroll reveal

--- js/script.js
+++ js/script.js
@@ -52,9 +52,9 @@ ScrollReveal({
 });
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
-ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
-ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .qualification__container, .skills__container', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img, .contact-info', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content, .contact form', { origin: 'right' });
 
 /*==================== TYPED JS ====================*/
 const typed = new Typed('.multiple-text', {
